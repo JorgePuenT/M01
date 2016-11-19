e378bb8276fd81792d28cc42271cc804a0f60953
Jorge Puente Zaro <jorgepuent@gmail.com>
2016-11-19 09:55:52 +0100

    gitignore modificado

--- src/main/java/ejemplo/App.java
+++ src/main/java/ejemplo/App.java
@@ -1,8 +1,13 @@
 package ejemplo;
+
+/**
+ * Hello world!
+ *
+ */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "¡Hola mundo!" );
+        System.out.println( "Hello World!" );
     }
 }
